ba2fcfb67344b1e12e18ded022e7fd0121ebbeac
ashuoshuo <11920570+ashuoshuo@users.noreply.github.com>
2021-08-03 21:03:03 -0400

    Check guid and CQ

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-// Based on https://github.com/jonhoo/rust-ibverbs/blob/master/examples/loopback.rs
-// and https://docs.rs/ibverbs/0.5.0/src/ibverbs/lib.rs.html#141
+// See https://github.com/jonhoo/rust-ibverbs/blob/master/examples/loopback.rs
+// See https://docs.rs/ibverbs/0.5.0/src/ibverbs/lib.rs.html
+// See https://docs.rs/ibverbs/0.6.0/src/ibverbs/lib.rs.html
 // Refer to https://github.com/jonhoo/rust-ibverbs/commit/cfbb93771fd180c63ca0ff89d80121674239be9f
 // for the start
 
@@ -9,8 +10,16 @@ fn proc_dev(dev: Option<Device>) {
     println!("Do something");
     match dev {
         Some(d) => {
-           let _ctx = d.open().unwrap();
-           println!("Check the ctx")
+            let guid_num = d.guid().unwrap();
+            println!("The guid {} ", guid_num);
+            let ctx = d.open().unwrap();
+            println!("Check the ctx ");
+            // create_cq  number of entries is 4 id is 1
+            let cq = ctx.create_cq(4, 1).unwrap();
+            let mut comp = [ibverbs::ibv_wc::default(); 1];
+            // See https://github.com/jonhoo/rust-ibverbs/blob/master/examples/loopback.rs from github
+            cq.poll(&mut comp);
+            println!("Try to poll");
         },
         None =>  println!("no device"),
     }
