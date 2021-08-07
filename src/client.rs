4c1c613616e85655ced2c225154d44e73a084e4f
ashuoshuo <11920570+ashuoshuo@users.noreply.github.com>
2021-08-07 18:53:20 -0400

    Show client IP address

--- src/client.rs
+++ src/client.rs
@@ -7,6 +7,8 @@ fn main() {
     loop {
         if let Ok(stream) = TcpStream::connect("127.0.0.1:8888") {
             println!("Connect set up!");
+            println!("my addr {} ", stream.local_addr().unwrap());
+            println!("remote addr {} ", stream.peer_addr().unwrap());
             break
         }
     }
