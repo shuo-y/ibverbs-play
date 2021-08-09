e623145257646a6fb403a5fe0751a8951716aee5
ashuoshuo <11920570+ashuoshuo@users.noreply.github.com>
2021-08-08 21:56:28 -0400

    Exchange TCP message

--- src/client.rs
+++ src/client.rs
@@ -1,14 +1,26 @@
 // Source https://doc.rust-lang.org/std/net/struct.TcpStream.html
+// Source https://doc.rust-lang.org/std/str/fn.from_utf8.html
 
 use std::net::TcpStream;
+use std::io::Write;
+use std::io::Read;
 
 fn main() {
 
     loop {
-        if let Ok(stream) = TcpStream::connect("127.0.0.1:8888") {
+        if let Ok(mut stream) = TcpStream::connect("127.0.0.1:8888") {
+            // Use mut stream see E00596
             println!("Connect set up!");
             println!("my addr {} ", stream.local_addr().unwrap());
             println!("remote addr {} ", stream.peer_addr().unwrap());
+            let request = "Msg from client";
+            stream.write(request.as_bytes()).unwrap();
+            stream.flush().unwrap(); // Why
+
+            let mut buffer = [0; 32];
+            stream.read(&mut buffer).unwrap();
+            let msg = std::str::from_utf8(&buffer).unwrap();
+            println!("Receive: {} ", msg);
             break
         }
     }
--- src/server.rs
+++ src/server.rs
@@ -3,15 +3,23 @@
 // Source https://doc.rust-lang.org/stable/std/time/struct.Duration.html
 // Source https://doc.rust-lang.org/book/ch20-01-single-threaded.html
 // Source https://doc.rust-lang.org/std/net/struct.TcpListener.html
+// Source https://doc.rust-lang.org/std/str/fn.from_utf8.html
 
 use std::net::TcpListener;
 use std::net::TcpStream;
 use std::io::Write;
+use std::io::Read;
 
 fn handle_con(mut stream: TcpStream) {
     // See https://www.reddit.com/r/rust/comments/8bak8k/help_getting_ip_address_client_connected_to_with/
     // See https://doc.rust-lang.org/std/net/struct.TcpStream.html
     println!("Try to response {} ", stream.peer_addr().unwrap());
+    let mut buffer = [0; 32];
+    stream.read(&mut buffer).unwrap();
+    println!("Received: {} ", std::str::from_utf8(&buffer).unwrap());
+
+    let response = "Msg from server";
+    stream.write(response.as_bytes()).unwrap();
 }
 
 fn main() {
