4f545870875e55d909f162987823abc622b7837f
ashuoshuo <11920570+ashuoshuo@users.noreply.github.com>
2021-08-15 20:36:05 -0400

    Use HashMap in Rust

--- src/mem.rs
+++ src/mem.rs
@@ -1,5 +1,7 @@
 // See https://doc.rust-lang.org/std/primitive.array.html
 
+use std::collections::HashMap;
+
 fn check_mem(array: &[i32; 10]) {
     for i in 0..10 {
         assert_eq!(array[i], 1);
@@ -10,4 +12,12 @@ fn main() {
     let array : [i32; 10] = [1; 10]; 
     println!("array {:?} ", array);
     check_mem(&array);
+
+    // See https://doc.rust-lang.org/book/ch08-03-hash-maps.html
+    let keys = vec![12];
+    let values = vec!["hello".to_string()];
+    let mut table: HashMap<_, _> =
+        keys.into_iter().zip(values.into_iter()).collect();
+
+    println!("{:?}", table);
 }
