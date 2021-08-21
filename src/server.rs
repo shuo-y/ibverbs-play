fc3c36b246b5b8e45b8f5be9827c951d54f3c647
ashuoshuo <11920570+ashuoshuo@users.noreply.github.com>
2021-08-20 22:29:59 -0400

    Convert u64 to u8 array

--- src/server.rs
+++ src/server.rs
@@ -9,22 +9,30 @@ use std::net::TcpListener;
 use std::net::TcpStream;
 use std::io::Write;
 use std::io::Read;
-
+use rand::Rng;
 use std::ptr;
+
+
 
 fn handle_con(mut stream: TcpStream) {
     // See https://www.reddit.com/r/rust/comments/8bak8k/help_getting_ip_address_client_connected_to_with/
     // See https://doc.rust-lang.org/std/net/struct.TcpStream.html
     println!("Try to response {} ", stream.peer_addr().unwrap());
     let mut buffer = [0; 32];
-    let inp = [0; 32];
+    let val: u64 = 10;
+    // See https://docs.rs/rand/0.6.2/rand/trait.Rng.html#method.gen
+    let inp = rand::thread_rng()
+        .gen::<u64>()
+        .to_be_bytes(); // See https://doc.rust-lang.org/std/primitive.u64.html#method.to_be_bytes
+    println!("inp {:?}", inp);
     // See https://doc.rust-lang.org/std/ptr/fn.copy_nonoverlapping.html
     unsafe {
         let dst = buffer.as_mut_ptr().offset(0);
         let src = inp.as_ptr();
 
-        ptr::copy_nonoverlapping(src, dst, 4);
+        ptr::copy_nonoverlapping(src, dst, 8);
     }
+    println!("buffer {:?}", buffer);
     stream.read(&mut buffer).unwrap();
     println!("Received: {} ", std::str::from_utf8(&buffer).unwrap());
 
