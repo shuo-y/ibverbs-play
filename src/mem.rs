589f8ba440a87fe16a91d5b8c805f573cc11e2a7
ashuoshuo <11920570+ashuoshuo@users.noreply.github.com>
2021-08-22 17:44:12 -0400

    Learn ownership

--- src/mem.rs
+++ src/mem.rs
@@ -4,6 +4,14 @@ use std::collections::HashMap;
 
 // About tree see https://gist.github.com/aidanhs/5ac9088ca0f6bdd4a370
 
+fn return_array(val: usize) -> [u32; 8] {
+    let mut ret = [0; 8];
+    for i in 0..val {
+        ret[i] = 10;
+    }
+    return ret;
+}
+
 fn main() {
     // See https://doc.rust-lang.org/book/ch08-03-hash-maps.html
     let keys = vec![12];
@@ -11,13 +19,8 @@ fn main() {
     let mut table: HashMap<_, _> =
         keys.into_iter().zip(values.into_iter()).collect();
     table.insert(11, "w".to_string());
-
-    // See https://doc.rust-lang.org/book/ch10-02-traits.html
-    let msg = format!("{:?}", table);
-    println!("msg{}", msg);
-
+    println!("{:?}", return_array(3));
     // See https://doc.rust-lang.org/book/ch16-01-threads.html
-
     let thread = std::thread::spawn( move || {
         table.insert(13, "zoo".to_string());
         println!("table[13] = {}", table[&13]);
