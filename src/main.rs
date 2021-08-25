d3b04b033fdf6382c9246a59db57270c75bf2971
ashuoshuo <11920570+ashuoshuo@users.noreply.github.com>
2021-08-25 18:25:01 -0400

    Update

--- src/main.rs
+++ src/main.rs
@@ -6,46 +6,12 @@
 // See https://doc.rust-lang.org/book/ch05-02-example-structs.html about #[derive(Debug)]
 
 use ibverbs::Device;
-use std::process::Command;
-
-fn proc_dev(dev: Option<Device>) {
-    println!("Do something");
-    match dev {
-        Some(d) => {
-            let guid_num = d.guid().unwrap();
-            println!("The guid {} ", guid_num);
-            let ctx = d.open().expect("cannot create ctx"); // Similar to unwrap
-            println!("Check the ctx ");
-            // create_cq  number of entries is 4 id is 1
-            let cq = ctx.create_cq(4, 1).unwrap();
-            let mut comp = [ibverbs::ibv_wc::default(); 1];
-            // See https://github.com/jonhoo/rust-ibverbs/blob/master/examples/loopback.rs from github
-            cq.poll(&mut comp);
-            println!("Try to poll");
-            let _pd = ctx.alloc_pd().unwrap();
-        },
-        None =>  println!("no device"),
-    }
-}
-
-// See https://doc.rust-lang.org/book/ch10-02-traits.html
-pub struct Log {
-    pub id: u32,
-    pub content: String,
-}
-
-impl Log {
-    fn getname(&self) -> String {
-        format!("{}.{}", self.id, self.content)
-    }
-}
-
+// use std::process::Command;
+use std::env;
+//use std::ffi::OsString;
 
 fn main() {
-    let devices = ibverbs::devices();
-
-    // Check the Result type
-    // See https://doc.rust-lang.org/rust-by-example/error/result/result_map.html
+    // Result type see https://doc.rust-lang.org/rust-by-example/error/result/result_map.html
     // Check the type https://stackoverflow.com/questions/21747136/how-do-i-print-the-type-of-a-variable-in-rust
     // See https://doc.rust-lang.org/std/any/fn.type_name.html
     // See https://doc.rust-lang.org/std/process/struct.Command.html
@@ -59,13 +25,8 @@ fn main() {
         x.len()
     };
     println!("{}", get_dev_num(&devices));
-    let tmp = get_dev(100);
-    let dev_guid = ibverbs::devices()
-        .unwrap()
-        .get(0)
-        .unwrap()
-        .guid()
-        .unwrap();
+    let dev = get_dev(0).unwrap();
+    let dev_guid = dev.guid().unwrap();
 
     println!("Dev guid {} ", dev_guid);
 
