f0cac1b36c2e6059f4f6761a1cd97e3eeccd479a
ashuoshuo <11920570+ashuoshuo@users.noreply.github.com>
2021-08-30 18:49:24 -0400

    Prepare TCP

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
 // See https://github.com/jonhoo/rust-ibverbs/blob/master/examples/loopback.rs
 // See https://docs.rs/ibverbs/0.5.0/src/ibverbs/lib.rs.html
 // See https://docs.rs/ibverbs/0.6.0/src/ibverbs/lib.rs.html
-// Refer to https://github.com/jonhoo/rust-ibverbs/commit/cfbb93771fd180c63ca0ff89d80121674239be9f
-// for the start
+// Refer to https://github.com/jonhoo/rust-ibverbs/commit/cfbb93771fd180c63ca0ff89d80121674239be9f for the start
 // See https://doc.rust-lang.org/book/ch05-02-example-structs.html about #[derive(Debug)]
-
-// use std::process::Command;
 
 fn main() {
     // 0 for server 1 for client
@@ -22,7 +19,6 @@ fn main() {
     // Result type see https://doc.rust-lang.org/rust-by-example/error/result/result_map.html
     // Check the type https://stackoverflow.com/questions/21747136/how-do-i-print-the-type-of-a-variable-in-rust
     // See https://doc.rust-lang.org/std/any/fn.type_name.html
-    // See https://doc.rust-lang.org/std/process/struct.Command.html
     // See rustc --explain E0515
     // See https://doc.rust-lang.org/book/ch13-01-closures.html
     let devices = ibverbs::devices().unwrap();
@@ -53,6 +49,13 @@ fn main() {
     let endpoint0_bytes = unsafe {
         std::mem::transmute::<ibverbs::QueuePairEndpoint, [u8; 24]>(endpoint0)
     };
+    let tcp_listen = std::net::TcpListener::bind("127.0.0.1:12345").unwrap();
+    let total_clients = 1;
+    // See https://doc.rust-lang.org/std/net/struct.TcpListener.html
+    for client_id in 0..total_clients {
+        let (_s, address) = tcp_listen.accept().unwrap();
+        println!("connect from addrss {:?} ", address);
+    }
     // QueuePairEndpoint 192 bits
     let pqp1 = pd0
         .create_qp(&send_cq, &recv_cq, ibverbs::ibv_qp_type::IBV_QPT_RC)
