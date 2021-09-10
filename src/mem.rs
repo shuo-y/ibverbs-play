10abb0284c3881b383735d169db0374b38a689de
ashuoshuo <11920570+ashuoshuo@users.noreply.github.com>
2021-09-09 22:50:23 -0400

    Use hash map

--- src/mem.rs
+++ src/mem.rs
@@ -4,24 +4,10 @@ use std::collections::HashMap;
 
 // About tree see https://gist.github.com/aidanhs/5ac9088ca0f6bdd4a370
 
-fn return_array(val: usize) -> [u32; 8] {
-    let mut ret = [0; 8];
-    for i in 1..val {
-        ret[i] = 10;
-        let p = &ret[i - 1];
-        println!("{}", *p);
-    }
-    return ret;
-}
-
 fn main() {
     // See https://doc.rust-lang.org/book/ch08-03-hash-maps.html
-    let keys = vec![12];
-    let values = vec!["hello".to_string()];
-    let mut table: HashMap<_, _> =
-        keys.into_iter().zip(values.into_iter()).collect();
+    let mut table: HashMap<u32, String> = std::collections::HashMap::new();
     table.insert(11, "w".to_string());
-    println!("{:?}", return_array(3));
     // See https://doc.rust-lang.org/book/ch16-01-threads.html
     let thread = std::thread::spawn( move || {
         table.insert(13, "zoo".to_string());
